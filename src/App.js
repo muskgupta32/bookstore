2b36db176c05a1665380489c8cd742feb51c749b
muskgupta32 <muskgupta32@gmail.com>
2024-06-25 22:56:18 +0530

    eigth commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
      <BrowserRouter>
         <Routes>
           <Route path="/shoppingCart" element={<ShoppingCart/>}/>
-          <Route path="/" element={<Home/>}/>
+          <Route path="" element={<Home/>}/>
           <Route path="/bookstore/" element={<Home/>}/>
           <Route path="/login" element={<Login/>}/>
 
