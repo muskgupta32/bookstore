bd8124cb4d56a75a90406caee06e6c1f264bdd59
muskgupta32 <muskgupta32@gmail.com>
2024-06-26 00:27:00 +0530

    th commit

--- src/NavLogin.js
+++ src/NavLogin.js
@@ -1,43 +1,47 @@
-import React,{ useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-function NavLogin() {
-  let navigate = useNavigate();
-  const cartProductsRedux = useSelector((output) => {
-    console.log('navbar products:', output.shoppingCart.cartProducts);
-    return output.shoppingCart.cartProducts;
-  });
 
-  const savedCartProducts = localStorage.getItem('cartProducts');
-  const initialCartProducts = cartProductsRedux.length > 0 ? cartProductsRedux : JSON.parse(savedCartProducts);
+function NavLogin() {
+  const navigate = useNavigate();
+  
+  // Using useSelector to get cartProductsRedux from Redux store
+  const cartProductsRedux = useSelector((state) => state.shoppingCart.cartProducts);
+
+  // Initialize cartProducts with empty array if cartProductsRedux is null or undefined
+  const initialCartProducts = cartProductsRedux || [];
+
+  // State to manage cart products locally
   const [cartProducts, setCartProducts] = useState(initialCartProducts);
 
+  // Effect to sync cartProducts with localStorage whenever it changes
   useEffect(() => {
-    // Save cartProducts to local storage whenever it changes
     localStorage.setItem('cartProducts', JSON.stringify(cartProducts));
   }, [cartProducts]);
 
-
-  
   return (
     <div>
-      <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-  <div class="container">
-    <a onClick={()=>{ navigate("/")}} class="navbar-brand fw-bold fs-4"  href="#">BOOKSTORE</a>
-   
-   <div className='buttons'>
-   <Link to="/shoppingCart" className='btn btn-outline-light ms-2'>
+      <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div className="container">
+          <a onClick={() => navigate("/")} className="navbar-brand fw-bold fs-4" href="#">
+            BOOKSTORE
+          </a>
+          <div className='buttons'>
+            <Link to="/shoppingCart" className='btn btn-outline-light ms-2'>
               <i className='fa fa-shopping-cart me-1'></i>Cart ({cartProducts.length})
             </Link>
-    <a href='' onClick={()=>{ navigate("/login")}} className='btn btn-outline-light ms-2'><i className='fa fa-sign-in me-1'></i>Login</a>
-    <a href='' onClick={()=>{ navigate("/signup")}} className='btn btn-outline-light ms-2'><i className='fa fa-user-plus me-1'></i>Sign Up</a>
-   
-   </div>
-  </div>
-</nav>
+            <a href='' onClick={() => navigate("/login")} className='btn btn-outline-light ms-2'>
+              <i className='fa fa-sign-in me-1'></i>Login
+            </a>
+            <a href='' onClick={() => navigate("/signup")} className='btn btn-outline-light ms-2'>
+              <i className='fa fa-user-plus me-1'></i>Sign Up
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default NavLogin;
